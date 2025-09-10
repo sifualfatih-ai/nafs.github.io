f0620727a730ef50157473c916813737ffd17d56
nnafss <sitimulyati.alfatih@gmail.com>
2025-09-11 05:31:20 +0700

    Update vite.config.js

--- react/vite/vite.config.js
+++ react/vite/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// App kamu diakses di https://nafsflow.com/react/vite/
 export default defineConfig({
   plugins: [react()],
-  base: "/react/vite/",           // penting untuk GitHub Pages subfolder
+  base: "/react/vite/",
 });
