dafdc16b72088f2f2c17d6dbe35697308bab0c37
nnafss <sitimulyati.alfatih@gmail.com>
2025-09-11 04:52:34 +0700

    Create HomeView.jsx

---
+++ react/vite/src/views/HomeView.jsx
@@ -0,0 +1,10 @@
+export default function HomeView() {
+  return (
+    <div className="flex-1 grid place-items-center p-6">
+      <div className="text-center text-white/70">
+        <div className="text-2xl mb-2">Halaman Utama</div>
+        <p>Halaman ini biasanya diarahkan ke situs utama lewat tombol sidebar.</p>
+      </div>
+    </div>
+  );
+}
