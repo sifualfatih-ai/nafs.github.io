d0fc675a9ea6dbae3954a6b0b05b7eeb68eae9b9
nnafss <sitimulyati.alfatih@gmail.com>
2025-09-11 12:55:52 +0700

    Update vite.config.js

--- react/vite/vite.config.js
+++ react/vite/vite.config.js
@@ -1,3 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-export default defineConfig({ plugins:[react()], base:"/react/vite/" });
+
+export default defineConfig({
+  plugins: [react()],
+  base: "/react/vite/",
+});
