f6f753296e583e01c1fe18a0c2703605cecc0779
nnafss <sitimulyati.alfatih@gmail.com>
2025-09-14 17:15:03 +0700

    Update firebase.js

--- guru/rpm/firebase.js
+++ guru/rpm/firebase.js
@@ -4,10 +4,13 @@
 
 // 1) CONFIG Firebase (PUBLIC – aman ditaruh di client)
 export const firebaseConfig = {
-  apiKey: "PASTE_API_KEY",
-  authDomain: "your-project-id.firebaseapp.com",
-  projectId: "your-project-id",
-  // (opsional) storageBucket, messagingSenderId, appId
+apiKey: "AIzaSyDRroJEkBTlADbB7C504ltyy3pG6kpMK04",
+  authDomain: "nafs-gen.firebaseapp.com",
+  projectId: "nafs-gen",
+  storageBucket: "nafs-gen.firebasestorage.app",
+  messagingSenderId: "514651223524",
+  appId: "1:514651223524:web:367229fb2c5ece794583f1",
+  measurementId: "G-0948R8WGQG"
 };
 
 // 2) Import SDK dari CDN (module)
