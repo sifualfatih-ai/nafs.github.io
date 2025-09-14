0c08fbc70689f8784a7efed2173befa305bf3f14
nnafss <sitimulyati.alfatih@gmail.com>
2025-09-14 21:54:29 +0700

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,12 +1,13 @@
 // /firebase.js
 export const firebaseConfig = {
-  apiKey : "AIzaSyDRroJEkBTlADbB7C504ltyy3pG6kpMK04" , 
-  authDomain : "nafs-gen.firebaseapp.com" , 
-  projectId : "nafs-gen" , 
-  storageBucket : "nafs-gen.firebasestorage.app" , 
-  messageSenderId : "514651223524" , 
-  appId : "1:514651223524:web:367229fb2c5ece794583f1" , 
-  ID pengukuran : "G-0948R8WGQG" 
+ const firebaseConfig = {
+  apiKey: "AIzaSyDRroJEkBTlADbB7C504ltyy3pG6kpMK04",
+  authDomain: "nafs-gen.firebaseapp.com",
+  projectId: "nafs-gen",
+  storageBucket: "nafs-gen.firebasestorage.app",
+  messagingSenderId: "514651223524",
+  appId: "1:514651223524:web:367229fb2c5ece794583f1",
+  measurementId: "G-0948R8WGQG"
 };
 
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
